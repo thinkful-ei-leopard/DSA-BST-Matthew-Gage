0aa953d23287e5bfb3dba05a24c8f139b7eee340
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-07 15:16:42 -0500

    Finished

--- BST-drills.js
+++ BST-drills.js
@@ -38,17 +38,45 @@ function thirdLargest(tree) {
   }
 }
 
+function isBalancedBST(tree) {
+  if (!tree) {
+    return true;
+  }
+  let heightDifference = Math.abs(treeHeight(tree.left) - treeHeight(tree.right));
+  if (heightDifference > 1) {
+    return false;
+  } else {
+    return isBalancedBST(tree.left) && isBalancedBST(tree.right);
+  }
+}
+
+function isOneOfTheseThingsNotLikeTheOther(arr1, arr2) {
+  if (arr1[0] != arr2[0]) {
+    return false;
+  }
+  console.log(arr1.sort(), arr2.sort());
+  if (arr1.sort().join(',') === arr2.sort().join(',')) {
+    return true;
+  } else {
+    return 'Wat';
+  }
+
+}
+
 function main() {
   let bst = new BST;
+  
   bst.insert(3);
   bst.insert(6);
   bst.insert(4);
   bst.insert(5);
   bst.insert(7);
+  bst.insert(8);
+  bst.insert(9);
   bst.insert(1);
   bst.insert(0);
   bst.insert(2);
-  return thirdLargest(bst).key;
+  return isOneOfTheseThingsNotLikeTheOther([3, 1, 5, 2, 4, 6, 0], [3, 5, 4, 6, 1, 0, 2]);
 }
 
 console.log(main());
